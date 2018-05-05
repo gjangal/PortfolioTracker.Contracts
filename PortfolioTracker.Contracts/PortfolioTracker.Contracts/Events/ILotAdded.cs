b6d67147ed7d3ed8ea053939fc76e4a7a19d78ad
Ganesh Jangal <ganesh.jangal@outlook.com>
2018-05-05 19:38:07 -0400

    Added contracts for Lot and Cash Events. Consumers in the HistoryService will respond to this along with daily calculation. WIP

---
+++ PortfolioTracker.Contracts/PortfolioTracker.Contracts/Events/ILotAdded.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioTracker.HistoryService.Contracts
+{
+    public interface ILotAdded
+    {
+        int PortfolioId { get; set; }
+        int LotId       { get; set; }
+        DateTime Date   { get; set; }
+        int Qty         { get; set; }
+        decimal Amount  { get; set; }
+        string Ric      { get; set; }
+    }
+}
---
+++ PortfolioTracker.Contracts/PortfolioTracker.Contracts/Events/ILotSold.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioTracker.HistoryService.Contracts
+{
+    interface ILotSold
+    {
+        int PortfolioId { get; set; }
+        int LotId { get; set; }
+        DateTime Date { get; set; }
+        int Qty { get; set; }
+        decimal Amount { get; set; }
+        string Ric { get; set; }
+    }
+}
